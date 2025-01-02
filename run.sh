3aace47ad8c8dec7aab87866c0a38403ed5ae349
Godai Takashina <azarashi.azarashi.a@gmail.com>
2025-01-02 11:30:34 +0900

    initial commit

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Build EFI image
+cargo build --target x86_64-unknown-uefi
+
+# Make EFI system partition
+mkdir -p esp/efi/boot
+cp target/x86_64-unknown-uefi/debug/mikanos-rs-loader.efi esp/efi/boot/bootx64.efi
+
+# Launch VM
+qemu-system-x86_64 \
+  -drive if=pflash,format=raw,readonly=on,file=assets/OVMF_CODE.fd \
+  -drive if=pflash,format=raw,readonly=on,file=assets/OVMF_VARS.fd \
+  -drive format=raw,file=fat:rw:esp
