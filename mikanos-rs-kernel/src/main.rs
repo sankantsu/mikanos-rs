0eee790993194d22793fb03d4185cfec7c9aa2da
datahaikuninja <jundai.takashina@gmail.com>
2025-01-04 16:15:48 +0900

    fix: draw pixel with kernel.
    move drawing routine from boot loader to kernel.

--- mikanos-rs-kernel/src/main.rs
+++ mikanos-rs-kernel/src/main.rs
@@ -2,6 +2,7 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+use core::slice;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -9,6 +10,15 @@ fn panic(_info: &PanicInfo) -> ! {
 }
 
 #[unsafe(no_mangle)]
-pub extern "C" fn kernel_main() {
-    loop {}
+pub extern "C" fn kernel_main(frame_buffer_base: *mut u8, frame_buffer_size: usize) {
+    let mut cnt = 0;
+    loop {
+        let buf = unsafe { slice::from_raw_parts_mut(frame_buffer_base, frame_buffer_size) };
+        if cnt % 2 == 0 {
+            buf.fill(0);
+        } else {
+            buf.fill(255);
+        }
+        cnt += 1;
+    }
 }
