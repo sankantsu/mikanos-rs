636f54e96890932eac2893adabae8fd6c6396c46
datahaikuninja <jundai.takashina@gmail.com>
2025-01-18 18:10:46 +0900

    remove: stop writing memmap file.

--- mikanos-rs-loader/src/main.rs
+++ mikanos-rs-loader/src/main.rs
@@ -2,19 +2,17 @@
 #![no_main]
 
 extern crate alloc;
-use alloc::{format, vec, vec::Vec};
+use alloc::{vec, vec::Vec};
 
 use core::slice;
 
 use goblin::elf;
 use log::info;
-use uefi::mem::memory_map::{MemoryMap, MemoryMapOwned};
+use uefi::mem::memory_map::MemoryMapOwned;
 use uefi::prelude::*;
 use uefi::proto::console::gop::GraphicsOutput;
 use uefi::proto::loaded_image::LoadedImage;
-use uefi::proto::media::file::{
-    Directory, File, FileAttribute, FileHandle, FileInfo, FileMode, RegularFile,
-};
+use uefi::proto::media::file::{Directory, File, FileAttribute, FileInfo, FileMode, RegularFile};
 use uefi::proto::media::fs::SimpleFileSystem;
 
 use mikanos_rs_frame_buffer::FrameBuffer;
@@ -24,32 +22,6 @@ fn open_root_dir() -> uefi::Result<Directory> {
     let device_handle = loaded_image.device().expect("Device handle should exist.");
     let mut fs = boot::open_protocol_exclusive::<SimpleFileSystem>(device_handle)?;
     fs.open_volume()
-}
-
-fn save_memory_map(file: FileHandle) -> uefi::Result {
-    let mut file = file.into_regular_file().unwrap();
-
-    // Print header
-    let header = "Index, Type, Type(name), PhysicalStart, NumberOfPages, Attribute\n";
-    file.write(header.as_bytes()).unwrap();
-
-    let memory_map = boot::memory_map(boot::MemoryType::LOADER_DATA)?;
-    for (i, desc) in memory_map.entries().enumerate() {
-        file.write(
-            format!(
-                "{}, {:#x}, {:?}, {:#08x}, {}, {:#x}\n",
-                i,
-                desc.ty.0,
-                desc.ty,
-                desc.phys_start,
-                desc.page_count,
-                desc.att.bits() & 0xfffff,
-            )
-            .as_bytes(),
-        )
-        .unwrap();
-    }
-    Ok(())
 }
 
 fn read_file(file: &mut RegularFile) -> uefi::Result<Vec<u8>> {
@@ -154,14 +126,6 @@ fn main() -> Status {
     info!("Hello, mikanos-rs!");
 
     let mut root_dir = open_root_dir().expect("Failed to open root directory.");
-    let memmap_file = root_dir
-        .open(
-            cstr16!("\\memmap"),
-            FileMode::CreateReadWrite,
-            FileAttribute::empty(),
-        )
-        .expect("Failed to open memmap file.");
-    save_memory_map(memmap_file).expect("Failed to save memory map.");
 
     let mut gop = open_gop().expect("Failed to open gop.");
     let frame_buffer = FrameBuffer::new(&mut gop);
