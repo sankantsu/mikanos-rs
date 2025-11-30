1bd8ea0c3c989552b062d6e77bb074e7a2081ed3
Godai Takashina <azarashi.azarashi.a@gmail.com>
2025-11-30 22:38:53 +0900

    refactor: Move console to a dedicated module.

--- mikanos-rs-kernel/src/main.rs
+++ mikanos-rs-kernel/src/main.rs
@@ -4,6 +4,7 @@
 extern crate alloc;
 
 mod allocator;
+mod console;
 mod descriptor;
 mod event;
 #[allow(static_mut_refs)]
@@ -19,6 +20,7 @@ mod task;
 mod timer;
 mod xhci;
 
+use console::Console;
 use core::panic::PanicInfo;
 use interrupt::enable_maskable_interrupts;
 use mikanos_rs_frame_buffer::{FrameBuffer, PixelColor};
@@ -48,73 +50,6 @@ impl KernelStack {
 }
 
 const _KERNEL_MAIN_STACK: KernelStack = KernelStack::new();
-
-struct Console {
-    frame_buffer: &'static FrameBuffer,
-    fg_color: PixelColor,
-    bg_color: PixelColor,
-    cursor_row: usize,
-    cursor_col: usize,
-    buffer: [[u8; Console::N_COLS]; Console::N_ROWS],
-}
-
-impl Console {
-    const N_ROWS: usize = 37;
-    const N_COLS: usize = 100;
-    pub fn new(
-        frame_buffer: &'static FrameBuffer,
-        fg_color: PixelColor,
-        bg_color: PixelColor,
-    ) -> Self {
-        Self {
-            frame_buffer,
-            fg_color,
-            bg_color,
-            cursor_row: 0,
-            cursor_col: 0,
-            buffer: [[0; Self::N_COLS]; Self::N_ROWS],
-        }
-    }
-    pub fn put_string(&mut self, s: &str) {
-        for c in s.as_bytes() {
-            if *c == b'\n' {
-                self.new_line();
-                continue;
-            }
-            self.write_byte(*c);
-        }
-    }
-    fn write_byte(&mut self, b: u8) {
-        let x = 8 * self.cursor_col;
-        let y = 16 * self.cursor_row;
-        self.frame_buffer.write_ascii(x, y, b, &self.fg_color);
-        self.buffer[self.cursor_row][self.cursor_col] = b;
-        self.cursor_col += 1;
-        if self.cursor_col == Self::N_COLS {
-            self.new_line();
-        }
-    }
-    fn new_line(&mut self) {
-        self.cursor_col = 0;
-        if self.cursor_row < Self::N_ROWS - 1 {
-            self.cursor_row += 1;
-        } else {
-            self.scroll_line();
-        }
-    }
-    fn scroll_line(&mut self) {
-        self.frame_buffer.fill(&self.bg_color);
-        self.cursor_col = 0;
-        self.cursor_row = 0;
-        for row in 0..(Self::N_ROWS - 1) {
-            self.buffer[row] = self.buffer[row + 1];
-            for col in 0..Self::N_COLS {
-                self.write_byte(self.buffer[row][col]);
-            }
-        }
-        self.buffer[Self::N_ROWS - 1].fill(0);
-    }
-}
 
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn kernel_main(
